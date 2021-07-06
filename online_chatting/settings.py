23bae2c987aa200e7028b7c7e75712c2fd927e19
Catherine Li <lixiaoyin5@hotmail.com>
2021-07-06 12:45:09 +0800

    using django template

--- online_chatting/settings.py
+++ online_chatting/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'online_chatting.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'chat/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
